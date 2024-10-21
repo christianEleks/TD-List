0a3adb6c4c06db1fe11b7aee1426d77d6dc381f2
Christian Elekwachi <elekwachichristian464@gmail.com>
2024-10-21 09:07:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("input");
 const mee = document.querySelector(".mee")
 const hat = document.querySelector(".hat")
+container = document.querySelector(".container")
 
 
 mee.addEventListener("click", () => {
@@ -20,17 +21,30 @@ mee.addEventListener("click", () => {
         input.value = ''
 
         saveData();
+        myFun()
     
     }
  })
+function myFun(){
+  const countItem = hat.querySelectorAll("li")
+    if(countItem.length > 4){
+        container.style.height = "100%"
+    }
+    else{
+        container.style.height = ""
+    }
+}
+
  hat.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
      e.target.classList.toggle("checked")
      saveData();
+     myFun()
     }
     else if(e.target.tagName === "SPAN"){
      e.target.parentElement.remove()
      saveData();
+     myFun()
     }
   }, false);
 
@@ -40,6 +54,7 @@ mee.addEventListener("click", () => {
   
   function showList(){
     hat.innerHTML = localStorage.getItem("data")
+    myFun()
   }
  
   showList();
